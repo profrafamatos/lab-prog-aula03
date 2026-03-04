83b3c1c93be0ac3abef4cad61a13a2b33108c33d
Rafaella Matos <163598367+profrafamatos@users.noreply.github.com>
2026-03-04 12:05:15 +0000

    microexercicio07 operadores logicos

---
+++ microexercicio07.py
@@ -0,0 +1,8 @@
+nome_usuario = True
+senha = False
+eh_verificado = True
+
+if nome_usuario and senha and eh_verificado:
+    print("Login realizado com sucesso!")
+else: 
+    print("Falha no login")
